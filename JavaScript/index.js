701f42c653b1e827400c32b2a5c2e695271d0f36
Night-Wisp <RealNightWisp@gmail.com>
2018-04-14 15:20:19 -0600

    Update index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -6,5 +6,9 @@ document.getElementById("hidden").style.display='none';
 }
 
 function coords(event) {
+  var shift = "Shift key not pressed";
+  if (event.shiftKey == 1) {
+   shift = "Shift key pressed"; 
+  }
   alert ("X: " + event.clientX + "\nY: " + event.clientY + "\nYou clicked: " + event.button);
 }
