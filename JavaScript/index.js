a8b4b5a09a3f87d71e7db89aca6edc6b617cb8c7
Night-Wisp <RealNightWisp@gmail.com>
2018-04-14 15:04:31 -0600

    Update index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -4,3 +4,7 @@ document.getElementById("hidden").style.display='block';
 function hide() {
 document.getElementById("hidden").style.display='none';
 }
+
+function coords(event) {
+  alert ("X: " + event.clientX + "/nY: " + event.clientY + "/nYou clicked: " + event.button);
+}
