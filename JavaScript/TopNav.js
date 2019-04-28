09a18fc345003dfdd7fdbda079ab60e613ef2ab7
Night-Wisp <nwisp@icloud.com>
2019-04-27 19:20:14 -0600

    Update TopNav.js

--- JavaScript/TopNav.js
+++ JavaScript/TopNav.js
@@ -2,14 +2,14 @@
   var nav_links = ["index.html"];
   
   var style = document.createElement('link');
-  style.rel = "stylesheet"; style.type = "text/css"; style.href = "night-wisp.github.io/CSS/topbar.css";
+  style.rel = "stylesheet"; style.type = "text/css"; style.href = "https://night-wisp.github.io/CSS/topbar.css";
   
   var nav_bar = document.createElement('div');
   nav_bar.class = "topnav"; nav_bar.id = "myTopnav";
   
   nav_links.forEach(function (url) {
     var link = document.createElement('a');
-    link.class = "A"; link.href = "night-wisp.github.io/" + url;
+    link.class = "A"; link.href = "https://night-wisp.github.io/" + url;
     
     nav_bar.appendChild(link);
   });
