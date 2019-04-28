960564263df42d196afe8c0a63477b5f3c0ece18
Night-Wisp <nwisp@icloud.com>
2019-04-27 20:33:26 -0600

    Update TopNav.js

--- JavaScript/TopNav.js
+++ JavaScript/TopNav.js
@@ -1,19 +1,20 @@
 (function () {
-  var nav_links = ["index.html"];
+  var nav_links = [{url: "index.html", name: "Home"}, {url: "accounts.html", name: "Accounts"}];
   
   var style = document.createElement('link');
   style.rel = "stylesheet"; style.type = "text/css"; style.href = "https://night-wisp.github.io/CSS/topbar.css";
   
   var nav_bar = document.createElement('div');
-  nav_bar.class = "topnav"; nav_bar.id = "myTopnav";
+  nav_bar.classList.add("topnav"); nav_bar.id = "myTopnav";
   
-  nav_links.forEach(function (url) {
+  nav_links.forEach(function (data) {
     var link = document.createElement('a');
-    link.class = "A"; link.href = "https://night-wisp.github.io/" + url;
+    link.classList.add("A"); link.href = "https://night-wisp.github.io/" + data.url;
+    link.innerHTML = data.name;
     
     nav_bar.appendChild(link);
   });
   
-  var head = document.getElementsByTagName('head')[0], body = document.getElementsByTagName('body')[0];
-  head.appendChild(style); body.appendChild(nav_bar);
+  var head = document.getElementsByTagName('head')[0], body = document.getElementsByTagName('body')[0], body1 = body.children[0];
+  head.appendChild(style); body.insertBefore(nav_bar, body1);
 })();
